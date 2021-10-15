824102f5482e47f545bcb60d7c93a24acee10d01
Iroberts8 <iroberts@live.com.au>
2021-10-15 16:53:42 +1100

    Results and updated script

--- Scripts/Parks.R
+++ Scripts/Parks.R
@@ -82,6 +82,60 @@ write.csv(full_summ,file='Target summary')
 #My rough attempt at merging all richness files
 rich_nolarvae <- cbind(Ara_rich,Form_rich,Col_rich2,Bla_rich,Other_rich2,Ort_rich2)
 rich_nolarvae2 <- rich_nolarvae[,c(-3,-5,-7,-9,-11)]
-taxrich_full<- merge(taxrich,rich2,by='Pit_code', all.x=T, all.y=F)
-head(taxrich_full);dim(taxrich_full)
+taxrich_full2<- merge(taxrich,rich_nolarvae2,by='Pit_code', all.x=T, all.y=F)
+head(taxrich_full2);dim(taxrich_full2)
 
+dev.new(width=12,height=8,dpi=100,pointsize=16,noRStudioGD = T)
+par(mfrow=c(2,3),mar=c(4,4,1,1))
+boxplot(taxrich_full2$Ara_rich~taxrich_full2$Treatment+taxrich_full2$Year,ylab='Araneae richness',xlab='',xaxt='n',las=1)
+axis(side=1,at=1:4,labels=c('C_2016','R_2016','C_2019','R_2019'))
+
+boxplot(taxrich_full2$Form_rich~taxrich_full2$Treatment+taxrich_full2$Year,ylab='Formicidae richness',xlab='',xaxt='n',las=1)
+axis(side=1,at=1:4,labels=c('C_2016','R_2016','C_2019','R_2019'))
+
+boxplot(taxrich_full2$Col_rich2~taxrich_full2$Treatment+taxrich_full2$Year,ylab='Coleoptera richness',xlab='',xaxt='n',las=1)
+axis(side=1,at=1:4,labels=c('C_2016','R_2016','C_2019','R_2019'))
+
+boxplot(taxrich_full2$Bla_rich~taxrich_full2$Treatment+taxrich_full2$Year,ylab='Blattodea richness',xlab='',xaxt='n',las=1)
+axis(side=1,at=1:4,labels=c('C_2016','R_2016','C_2019','R_2019'))
+
+boxplot(taxrich_full2$Other_rich2~taxrich_full2$Treatment+taxrich_full2$Year,ylab='Other richness',xlab='',xaxt='n',las=1)
+axis(side=1,at=1:4,labels=c('C_2016','R_2016','C_2019','R_2019'))
+
+boxplot(taxrich_full2$Ort_rich2~taxrich_full2$Treatment+taxrich_full2$Year,ylab='Orthoptera richness',xlab='',xaxt='n',las=1)
+axis(side=1,at=1:4,labels=c('C_2016','R_2016','C_2019','R_2019'))
+
+#Add species diversity to main data file
+head(taxrich_full2);dim(taxrich_full2)
+head(Araneae[,1:10]);dim(Araneae)
+apply(X = Araneae[,6:ncol(Araneae)], MARGIN = 1, FUN = function(x) diversity(x,index="shannon",MARGIN=1))
+
+Ara_div <- data.frame(Pit_code=basedata$Pit_code, Ara_div=apply(X = Araneae[,6:ncol(Araneae)], MARGIN = 1, FUN = function(x) diversity(x,index="shannon",MARGIN=1)))
+head(Ara_div);dim(Ara_div)
+
+Bla_div <- data.frame(Pit_code=basedata$Pit_code, Bla_div=apply(X = Blattodea[,6:ncol(Blattodea)], MARGIN = 1, FUN = function(x) diversity(x,index="shannon",MARGIN=1)))
+head(Bla_div);dim(Bla_div)
+
+Col_div <- data.frame(Pit_code=basedata$Pit_code, Col_div=apply(X = Col2[,6:ncol(Col2)], MARGIN = 1, FUN = function(x) diversity(x,index="shannon",MARGIN=1)))
+head(Col_div);dim(Col_div)
+
+Form_div <- data.frame(Pit_code=basedata$Pit_code, Form_div=apply(X = Formicidae[,6:ncol(Formicidae)], MARGIN = 1, FUN = function(x) diversity(x,index="shannon",MARGIN=1)))
+head(Form_div);dim(Form_div)
+
+Other_div <- data.frame(Pit_code=basedata$Pit_code, Other_div=apply(X = Other2[,6:ncol(Other2)], MARGIN = 1, FUN = function(x) diversity(x,index="shannon",MARGIN=1)))
+head(Other_div);dim(Other_div)
+
+Ort_div <- data.frame(Pit_code=basedata$Pit_code, Ort_div=apply(X = Ort2[,6:ncol(Ort2)], MARGIN = 1, FUN = function(x) diversity(x,index="shannon",MARGIN=1)))
+head(Ort_div);dim(Ort_div)
+
+#Merging of the diversity data (Shannons)
+div <- cbind(Ara_div,Form_div,Col_div,Bla_div,Other_div,Ort_div)
+div_full <- div[,c(-3,-5,-7,-9,-11)]
+div_full2 <- merge(taxrich,div_full,by='Pit_code', all.x=T, all.y=F)
+head(div_full2);dim(div_full2)
+
+#Merging of diversity and richness
+div_rich <- cbind(div_full,rich_nolarvae2)
+div_rich2 <- div_rich[,c(-8)]
+div_rich_full2<- merge(taxrich,div_rich2,by='Pit_code', all.x=T, all.y=F)
+head(div_rich_full2);dim(div_rich_full2)
